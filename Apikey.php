f952500613c07c78dcea73b50a775a0021940a2f
Zhy_08 <31594965+Zhy-08scure@users.noreply.github.com>
2024-06-06 14:56:51 +0800

    Update Apikey.php

--- Apikey.php
+++ Apikey.php
@@ -1,29 +1,4 @@
 <?php
-function index($data_array) {
-   for ($i = 0; $i < count($data_array); $i++) {
-      $current_size = $data_array[$i];
-      $is_duplicate = false;
-      for ($j = 0; $j < count($data_array); $j++) {
-         if ($i != $j && $current_size == $data_array[$j]) {
-            $is_duplicate = true;
-            break;
-         }
-      }
-      if (!$is_duplicate) {
-         return $i;
-      }
-   }
-   return -1;
-}
-function icon() {
-   $cap = json_decode(roll(host.'system/libs/captcha/request.php', h(1), "cID=0&rT=1&tM=light")[1], 1);
-   foreach ($cap as $c) {
-      $im[] = strlen(base64_encode(roll(host.'system/libs/captcha/request.php?cid=0&hash='.$c, h(0, 1))[1]));
-   }
-   $no = index($im);
-   $res = roll(host.'system/libs/captcha/request.php', h(1), "cID=0&pC=".$cap[$no]."&rT=2")[1];
-   return $cap[$no];
-}
 function CheckApi() {
    print pan2.p."Register".sd.h.provider_ref.n;
    l();
@@ -287,15 +262,12 @@ Class Shortlinks {
 			"linksly" => "linksly",
 			"adcorto" => "adcorto",
 			"c2g" => "C2G",
-			"c2g link" => "C2G",
 			#"shrinkme" => "ShrinkMe",
 			#"shrinkearn" => "ShrkEarn",
 			"urlhives" => "UrlHives",
 			"linkhives" =>"LinkHives",
 			#"shortsme" => "shortsme",
 			"adlink" => "adlink",
-			#"revcut" => "revcut",
-			#"cutlink" => "cutlink",
 			"ez4short" => "ez4short"
 		];
 		sleep(1);
